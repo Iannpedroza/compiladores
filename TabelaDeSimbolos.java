e781fed55f87d5923556f15160aa6dc426e127b1
Iann Pedroza <33065016+Iannpedroza@users.noreply.github.com>
2020-04-21 14:15:00 -0300

    Update TabelaDeSimbolos.java

--- TabelaDeSimbolos.java
+++ TabelaDeSimbolos.java
@@ -89,20 +89,17 @@ public class TabelaDeSimbolos {
    }
 
    public String pesquisa(String lexema){
-        lexema = lexema.toLowerCase();
         Simbolo aux = tabela.get(lexema);
         return ((aux == null) ? "NULL" : ""+aux.getEnd());
    }
   
    public Simbolo inserirID(String lexema){
-       lexema = lexema.toLowerCase();
        Simbolo simbolo = new Simbolo(ID,lexema, ++endereco);
        tabela.put(lexema, simbolo);
        return tabela.get(lexema);
    }
     
    public Simbolo buscaSimbolo(String lexema){
-    lexema = lexema.toLowerCase();
     Simbolo aux = tabela.get(lexema);
     return ((aux == null) ? null : tabela.get(lexema));
 
@@ -110,7 +107,6 @@ public class TabelaDeSimbolos {
 
   
    public Simbolo inserirConst(String lexema, String tipo){
-       lexema = lexema.toLowerCase();
        Simbolo simbolo = new Simbolo(VALORCONST, lexema, ++endereco, tipo);
        tabela.put(lexema, simbolo);
        return tabela.get(lexema);
@@ -122,7 +118,8 @@ public class TabelaDeSimbolos {
        System.out.println(tbl.pesquisa("if"));
        System.out.println(tbl.pesquisa("FI"));
        System.out.println(tbl.pesquisa("iF"));
-       System.out.println(tbl.buscaSimbolo("and").getLexema());
+       System.out.println(tbl.buscaSimbolo("&&").getLexema());
+       System.out.println(tbl.buscaSimbolo("&&").getToken());
        System.out.println(tbl.pesquisa("And"));
        System.out.println(tbl.buscaSimbolo("for"));
        System.out.println(tbl.buscaSimbolo("<"));
@@ -133,89 +130,88 @@ public class TabelaDeSimbolos {
        System.out.println(tbl.pesquisa("teste"));
     }
 
-
+    class Simbolo {
+        private String lexema = "";
+        private byte token;
+        private String classe = "";
+        private String tipo = "";
+        private int tamanho = 0;
+        private int endereco;
+        
+        public Simbolo(){
+           this.token = -1;
+        }
+           
+        public Simbolo(byte token, String lexema, int endereco){
+           this.lexema = lexema;
+           this.token = token;
+           this.endereco = endereco;
+        }
+         
+        public Simbolo(byte token, String lexema,int endereco, String tipo){
+           this.token = token;
+           this.lexema = lexema;
+           this.tipo = tipo;
+           this.endereco = endereco;
+        }
+        
+        public Simbolo(byte token,String lexema, int endereco, String tipo,String classe, int tamanho) {
+           this.token = token;
+           this.lexema = lexema;
+           this.classe = classe;
+           this.tipo = tipo;
+           this.endereco = endereco;
+           this.tamanho = tamanho;
+        }
+     
+        public byte getToken(){
+           return token;
+        }
+         
+        public int getEnd(){
+           return endereco;
+        }
+         
+        public String getLexema(){
+           return lexema;
+        }
+     
+        public String getTipo() {
+           return tipo;
+        }
+     
+        public void setTipo(String tipo) {
+           this.tipo = tipo;
+        }
+        
+        public void setToken(byte token) {
+           this.token = token;
+        }
+        
+        public String getClasse() {
+           return classe;
+        }
+     
+        public void setClasse(String classe) {
+           this.classe = classe;
+        }
+     
+        public int getEndereco() {
+           return endereco;
+        }
+     
+        public void setEndereco(int endereco) {
+           this.endereco = endereco;
+        }
+        
+        public int getTamanho() {
+           return tamanho;
+        }
+     
+        public void setTamanho(int tamanho) {
+           this.tamanho = tamanho;
+        }
+     
+     }
 }
 
-class Simbolo {
-    private String lexema = "";
-    private byte token;
-    private String classe = "";
-    private String tipo = "";
-    private int tamanho = 0;
-    private int endereco;
-    
-    public Simbolo(){
-       this.token = -1;
-    }
-       
-    public Simbolo(byte token, String lexema, int endereco){
-       this.lexema = lexema;
-       this.token = token;
-       this.endereco = endereco;
-    }
-     
-    public Simbolo(byte token, String lexema,int endereco, String tipo){
-       this.token = token;
-       this.lexema = lexema;
-       this.tipo = tipo;
-       this.endereco = endereco;
-    }
-    
-    public Simbolo(byte token,String lexema, int endereco, String tipo,String classe, int tamanho) {
-       this.token = token;
-       this.lexema = lexema;
-       this.classe = classe;
-       this.tipo = tipo;
-       this.endereco = endereco;
-       this.tamanho = tamanho;
-    }
- 
-    public byte getToken(){
-       return token;
-    }
-     
-    public int getEnd(){
-       return endereco;
-    }
-     
-    public String getLexema(){
-       return lexema;
-    }
- 
-    public String getTipo() {
-       return tipo;
-    }
- 
-    public void setTipo(String tipo) {
-       this.tipo = tipo;
-    }
-    
-    public void setToken(byte token) {
-       this.token = token;
-    }
-    
-    public String getClasse() {
-       return classe;
-    }
- 
-    public void setClasse(String classe) {
-       this.classe = classe;
-    }
- 
-    public int getEndereco() {
-       return endereco;
-    }
- 
-    public void setEndereco(int endereco) {
-       this.endereco = endereco;
-    }
-    
-    public int getTamanho() {
-       return tamanho;
-    }
- 
-    public void setTamanho(int tamanho) {
-       this.tamanho = tamanho;
-    }
- 
- }
